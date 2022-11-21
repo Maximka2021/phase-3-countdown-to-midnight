15eaf09aa8b54edbe7b53a2c1fc60ceebd401263
Maxim Polyakov <polyakovmaxim05@gmail.com>
2022-11-21 10:22:03 -0500

    commit tree 05b371d40c35488907f6b4ab7c642fbfd499669f

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
+num = 10
+def countdown num
+    while num > 0
+        puts "#{num} SECOND(S)!"
+        num = num - 1
+    end
+     "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep num
+    while num > 0
+        puts "#{num} SECOND(S)!"
+        sleep 1
+        num = num - 1
+    end
+    "HAPPY NEW YEAR!"
 end
